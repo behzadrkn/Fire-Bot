fda375d31f8962f1b79b44d8bd41b374117f4d6c
behzadrkn <behzadrknp@gmail.com>
2016-03-10 01:53:53 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {147861394,tonumber(our_id)},--Sudo users
+    sudo_users = {147861394},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
